dabb426370d3158ffa4eb0a36a13cf1659db2176
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-01 21:44:54 +0530

    feat: add selection sort algorithm with user input and sorted array output

---
+++ selection_sort.c
@@ -0,0 +1,44 @@
+
+#include<stdio.h>
+
+void selection_sort (int arr[], int size) {
+
+  for (int i = 0 ; i < size - 1 ; i++){
+     
+      int minIndex = i ;
+      
+      for (int j = i ; j < size ; j++){
+          if (arr[j]<arr[minIndex]){
+                 minIndex = j;
+          }
+       }
+       
+       int temp = arr[i];
+       arr[i] = arr[minIndex];
+       arr[minIndex] = temp;
+    }
+
+}
+
+
+int main () {
+
+  int size;
+  printf("Enter size of the array : ");
+  scanf("%d",&size);
+  
+  int arr[size];
+  printf("Enter array elements : ");
+  for (int i = 0 ; i < size ; i++){
+     scanf("%d",&arr[i]);
+  }
+  
+  selection_sort(arr,size);
+
+    for (int i = 0 ; i < size ; i++){
+     printf("%d ",arr[i]);
+  }
+
+
+  return 0;
+}

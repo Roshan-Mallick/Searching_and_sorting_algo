b2a236811c1fa387cff42593ae27ad73d220e1d8
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-03 10:49:34 +0530

    feat(dsa): implement merge sort using divide and conquer

---
+++ merge_sort.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+void merge(int arr[] , int low , int mid , int high){
+
+  int temp[high - low + 1];
+  int left = low ;
+  int right = mid + 1;
+  int k = 0 ;
+  
+  while (left <= mid && right <= high ){ //merge both sorted arrays
+       if (arr[left]<= arr[right]){
+          temp[k++]=arr[left++];
+       }
+       else {
+           temp[k++]=arr[right++];
+       }
+  
+  }
+
+
+
+while (left <= mid ){ //copy remian elemnets from the left half 
+  temp[k++]=arr[left++];
+}
+
+while (right <= high){ // copy remianing elemnt form the left half 
+  temp[k++]=arr[right++];
+}
+
+
+for (int i = low ; i <= high ; i++ ){
+   arr[i] = temp [i - low];
+}
+
+
+}
+
+
+void merge_sort( int arr[] , int low , int high ){
+
+
+ if (low >= high){
+     return;
+ }
+ 
+ int mid = low + (high - low)/2;
+ 
+ merge_sort(arr,low,mid);
+ merge_sort(arr,mid+1,high);
+ merge (arr,low,mid,high);
+
+}
+
+
+int main () {
+
+  int size;
+  printf("Enter size of the array : ");
+  scanf("%d",&size);
+  
+  int arr[size];
+  
+  printf("Enter elements in the array : ");
+  for (int i = 0 ; i< size ; i++){
+     scanf("%d",&arr[i]);
+  }
+  
+  merge_sort(arr,0,size-1);
+  
+  
+
+  printf("Sorted array : ");
+  for (int i = 0 ; i< size ; i++){
+     printf("%d ",arr[i]);
+  }
+  
+
+
+ return 0;
+}
+
+
+
+
+
+
+

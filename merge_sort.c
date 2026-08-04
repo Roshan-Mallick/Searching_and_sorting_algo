4943271627191f34b25dc8d3ac0c63b34470d533
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-05 01:51:05 +0530

    Add Merge Sort implementation with user input

--- merge_sort.c
+++ merge_sort.c
@@ -1,87 +1,80 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void merge(int arr[] , int low , int mid , int high){
+void merge(int arr[], int low, int mid, int high)
+{
+    int temp[high - low + 1];
+    int left = low;
+    int right = mid + 1;
+    int k = 0;
 
-  int temp[high - low + 1];
-  int left = low ;
-  int right = mid + 1;
-  int k = 0 ;
-  
-  while (left <= mid && right <= high ){ //merge both sorted arrays
-       if (arr[left]<= arr[right]){
-          temp[k++]=arr[left++];
-       }
-       else {
-           temp[k++]=arr[right++];
-       }
-  
-  }
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp[k++] = arr[left++];
+        }
+        else
+        {
+            temp[k++] = arr[right++];
+        }
+    }
 
+    while (left <= mid)
+    {
+        temp[k++] = arr[left++];
+    }
 
+    while (right <= high)
+    {
+        temp[k++] = arr[right++];
+    }
 
-while (left <= mid ){ //copy remian elemnets from the left half 
-  temp[k++]=arr[left++];
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
 }
 
-while (right <= high){ // copy remianing elemnt form the left half 
-  temp[k++]=arr[right++];
+void merge_sort(int arr[], int low, int high)
+{
+    if (low == high)
+        return;
+
+    int mid = low + (high - low) / 2;
+
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
 }
 
+int main()
+{
+    int size;
+  
+    printf("Enter the size of array: ");
+    scanf("%d", &size);
 
-for (int i = low ; i <= high ; i++ ){
-   arr[i] = temp [i - low];
+    int arr[size];
+    
+    int low = 0;
+    int high = size - 1;
+
+    printf("Enter %d elements: ", size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    merge_sort(arr, low , high);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
 }
-
-
-}
-
-
-void merge_sort( int arr[] , int low , int high ){
-
-
- if (low >= high){
-     return;
- }
- 
- int mid = low + (high - low)/2;
- 
- merge_sort(arr,low,mid);
- merge_sort(arr,mid+1,high);
- merge (arr,low,mid,high);
-
-}
-
-
-int main () {
-
-  int size;
-  printf("Enter size of the array : ");
-  scanf("%d",&size);
-  
-  int arr[size];
-  
-  printf("Enter elements in the array : ");
-  for (int i = 0 ; i< size ; i++){
-     scanf("%d",&arr[i]);
-  }
-  
-  merge_sort(arr,0,size-1);
-  
-  
-
-  printf("Sorted array : ");
-  for (int i = 0 ; i< size ; i++){
-     printf("%d ",arr[i]);
-  }
-  
-
-
- return 0;
-}
-
-
-
-
-
-
-
